52f389907ef663ab91de9da181d86f5acaf094b0
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2024-10-08 23:20:49 +0530

    add Link using react-router-dom

--- src/pages/StartGame.jsx
+++ src/pages/StartGame.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom";
+import TextInputFormContainer from "../components/TextInputForm/TextInputFormContainer";
+
 function StartGame() {
-    <>
-        <h1>Start Game</h1>
-        
-    </>
+    return (
+        <>
+            <h1>Start Game</h1> 
+            <TextInputFormContainer />
+            <Link to='/play' className="text-blue-500">Play Game Link</Link>       
+        </>
+    );
 }
 
 export default StartGame;

4666f52fae2a7975b93f97676f749a4b9be683b0
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2024-10-08 23:21:42 +0530

    add Link using react-router-dom and create playgame page

---
+++ src/pages/PlayGame.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function PlayGame() {
+    return (
+        <>
+            <h1>Play Game</h1>
+            <Link to='/start' className="text-blue-500">Start Game Link</Link>       
+
+        </>
+    );
+}
+
+export default PlayGame;
